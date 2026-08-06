4e0b62abe39fca036279eeb6ca63fe1c95155948
jaidencoleflannery <jaidencoleflannery@gmail.com>
2026-08-05 22:02:30 -0500

    init | sync.

--- src/libfb/main.c
+++ src/libfb/main.c
@@ -2,11 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define WIDTH 320
+#define WIDTH  320
 #define HEIGHT 200
 
+uint32_t framebuffer[WIDTH * HEIGHT];
+
 int main(void) {
-    printf("Hello SDL.");
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Texture *texture;
+
+    window = SDL_CreateWindow(
+        "Framebuffer", 
+        WIDTH, 
+        HEIGHT, 
+        0
+    );
+
+    renderer = SDL_CreateRenderer(
+        window, 
+        NULL
+    );
+
+    texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_XRGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        WIDTH,
+        HEIGHT
+    );
 
     // initialize library.
     SDL_Init(SDL_INIT_VIDEO);
